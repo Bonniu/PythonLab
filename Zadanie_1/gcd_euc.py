2b2e10dc6930ca895192203cd58037655bedbece
Bonniu <radekg23@o2.pl>
2019-10-07 21:37:21 +0200

    Implemented Euclidean algorithm

--- Zadanie_1/gcd_euc.py
+++ Zadanie_1/gcd_euc.py
@@ -1,1 +1,12 @@
-print('test2')
+def nwd(a: int, b: int):
+    if a < 1 or b < 1:
+        return None
+    while b != 0:  # dopóki b nie jest 0 wykonuj
+        c = a % b  # reszta z dzielenia a przez b
+        a = b  # zastąpienie a liczbą b i b liczbą c
+        b = c
+    return a
+
+
+#  wyświetlenie wyniku dla liczb 84 i 18
+print("Wynik: ", nwd(84, 18))
