6f26e0e3a096b884d420e18fefae8b3322cc548c
216914 Wąchała Jakub <216914@edu.p.lodz.pl>
2019-10-09 16:13:09 +0200

    Dodanie metody ktora znajduje dzielniki daje liczby

--- Zadanie_1/lcm_prime.py
+++ Zadanie_1/lcm_prime.py
@@ -2,14 +2,13 @@ def lcm(a: [], b: []):
     index_b = 0
     result = []
     for i in range(len(a)):
-        print("index: ", index_b)
         if index_b == len(b):
             break
         if a[i] == b[index_b]:
-            b[index_b] = 0
+            b[index_b] = 1
             result.append(a[i])
             index_b += 1
-    return iloczyn(result)
+    return iloczyn(a) * iloczyn(b)
 
 
 def iloczyn(tab: []) -> int:
@@ -19,6 +18,30 @@ def iloczyn(tab: []) -> int:
     return tmp
 
 
-tab1 = [1, 2, 3, 5, 2, 3, 2, 3, 2]
-tab2 = [1, 2, 3, 2, 17, 17, 17]
-print(lcm(tab1, tab2))
+
+def dzielniki_liczby(x: int):
+    i=2
+    lista = []
+
+    while x != 1:
+        if x % i == 0:
+            lista.append(i)
+            x = x / i
+            i = 2
+
+        else: i=i+1
+
+    return lista
+
+
+
+tablica1 = dzielniki_liczby(192)
+tablica2 = dzielniki_liczby(348)
+print(lcm(tablica1,tablica2))
+print(tablica2)
+print(tablica1)
+
+
+
+
+
