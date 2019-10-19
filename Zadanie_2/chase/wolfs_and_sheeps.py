87276aca6db3732cfd8e6547e86f11833f91656c
Bonniu <radekg23@o2.pl>
2019-10-19 13:47:56 +0200

    Zadanie 2 - punkty 1, 2

--- Zadanie_2/chase/wolfs_and_sheeps.py
+++ Zadanie_2/chase/wolfs_and_sheeps.py
@@ -1,1 +1,110 @@
-print('start')
+import random
+from math import sqrt
+
+nr_of_rounds = 50
+nr_of_sheeps = 15
+init_pos_limit = 10.0
+sheep_move_dist = 0.5
+wolf_move_dist = 1.0
+sheeps: list = []
+
+
+class Sheep:
+    def __init__(self):
+        self.x = random.randint(-init_pos_limit * 100, init_pos_limit * 100) / 100
+        self.y = random.randint(-init_pos_limit * 100, init_pos_limit * 100) / 100
+        self.is_alive = True
+
+    def move_sheep(self):
+        direction = random.randint(1, 4)
+        if direction == 1:
+            self.y += sheep_move_dist
+        elif direction == 2:
+            self.x += sheep_move_dist
+        elif direction == 3:
+            self.y -= sheep_move_dist
+        else:
+            self.x -= sheep_move_dist
+
+    def kill_sheep(self):
+        self.is_alive = False
+
+    def __repr__(self):
+        return "Sheep[x=" + str(self.x) + ", y=" + str(self.y) + ", is_alive=" + str(self.is_alive) + "]"
+
+
+class Wolf:
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+
+    def move_wolf(self, sheep: Sheep):
+        vector = (sheep.x - self.x, sheep.y - self.y)
+        self.x += vector[0] * wolf_move_dist / calc_distance(self, sheep)
+        self.y += vector[1] * wolf_move_dist / calc_distance(self, sheep)
+
+    def __repr__(self):
+        return "Wolf[x=" + str(self.x) + ", y=" + str(self.y) + "]"
+
+
+def init_sheeps():
+    for i in range(nr_of_sheeps):
+        sheeps.append(Sheep())
+
+
+def calc_distance(w: Wolf, s: Sheep):
+    return sqrt((w.x - s.x) ** 2 + (w.y - s.y) ** 2)
+
+
+def print_sheeps(only_alive: bool):
+    for i in range(len(sheeps)):
+        if only_alive:
+            if sheeps[i].is_alive:
+                print(i, " - ", sheeps[i])
+        else:
+            print(i, " - ", sheeps[i])
+
+
+def alive_sheeps() -> int:
+    counter = 0
+    for i in sheeps:
+        if i.is_alive:
+            counter += 1
+    return counter
+
+
+def simulate():
+    init_sheeps()
+    print_sheeps(only_alive=False)
+    wolf = Wolf()
+    round = 0
+    while round != nr_of_rounds and alive_sheeps() > 0:
+        #  ruch owiec
+        for i in sheeps:
+            i.move_sheep()
+
+        #  ruch wilka
+        closest_sheep_index = 0
+        closest_sheep_dist = 1000000
+
+        for i in range(len(sheeps)):
+            if calc_distance(wolf, sheeps[i]) < closest_sheep_dist and sheeps[i].is_alive:
+                closest_sheep_index = i
+                closest_sheep_dist = calc_distance(wolf, sheeps[i])
+
+        #  zjedzenie lub ruch wilka
+        if closest_sheep_dist < wolf_move_dist:
+            sheeps[closest_sheep_index].kill_sheep()
+            print("Zjedzona owca nr", closest_sheep_index)
+        else:
+            wolf.move_wolf(sheeps[closest_sheep_index])
+
+        #  informacje o turze
+        print("Tura", round, "/", nr_of_rounds - 1, " Pozycja wilka: (", str(wolf.x), ",",
+              str(wolf.y), ")", " Ilość żywych owiec: ", alive_sheeps(), "\n")
+        round += 1
+    print_sheeps(only_alive=True)
+
+
+if __name__ == "__main__":
+    simulate()
